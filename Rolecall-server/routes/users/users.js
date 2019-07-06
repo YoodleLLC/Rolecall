978d56485ae29c290677ad1506bd5831a68cde5b
NehaNavgale <46933061+NehaNavgale@users.noreply.github.com>
2019-07-06 21:15:02 +0530

    Updated code for mongo v3

--- Rolecall-server/routes/users/users.js
+++ Rolecall-server/routes/users/users.js
@@ -3,47 +3,46 @@ var usersRouter = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var cors = require('cors');
 var mongo=require('mongodb');
+
 //connectionString="mongodb://rolecallpro:rolecallpro@rolecallpro-shard-00-00-5cpm4.mongodb.net:27017,rolecallpro-shard-00-01-5cpm4.mongodb.net:27017,rolecallpro-shard-00-02-5cpm4.mongodb.net:27017/rolecallpro?ssl=true&replicaSet=RolecallPro-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 connectionString = "mongodb+srv://rolecallpro:rolecallpro@rolecallpro-5cpm4.mongodb.net/test?retryWrites=true&w=majority";
 
-// Get  user by id
+const dbName = "rolecallpro";
+
 usersRouter.get('/all', function (req, res) {
     //connect to Mongo Client
-    MongoClient.connect(connectionString).
-    then(function (db) {
-        db.collection("users").find({}).toArray().
-        then(function (data, err) {
-            console.dir(data[0].fname)
-            res.send(data[0].fname)
-        });
-    }).catch(err => {
-        console.log(err)
-        res.send(409, err)
+    MongoClient.connect(connectionString, { useNewUrlParser: true }, function (err, client) {
+        const db = client.db(dbName);
+        // const collection = db.collection("users");
+            db.collection("users").find({}).toArray().
+            then(function (data, err) {
+                console.dir(data);
+                res.send(data);
+            });
     });
 });
 
 usersRouter.post('/adduser', function (req, res) {
-    MongoClient.connect(connectionString).
-    then(function (db) {
+    MongoClient.connect(connectionString, { useNewUrlParser: true }, function (err, client) {
+        const db = client.db(dbName);
         db.collection("users").insert(req.body.user, (err, res) => {
             if (err) throw err;
             console.log("User added successfully");
             db.close();
         });
-    }).catch(err => console.log(err));
-
+    });
 });
 
 usersRouter.post('/updateuser', function (req, res) {
-    MongoClient.connect(connectionString).
-    then(function (db) {
+    MongoClient.connect(connectionString, { useNewUrlParser: true }, function (err, client) {
+        const db = client.db(dbName);
         db.collection("users").updateOne(req.body.user._id, req.body.user, (err, res) => {
             if (err) throw err;
             console.log("User details updated");
             db.close();
         });
-    }).catch(err => console.log(err));
+    });
 });
 
 module.exports = usersRouter;
